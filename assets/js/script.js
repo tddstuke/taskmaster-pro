ed23cdf7cbfd598e5bde6564514f0f0ba1e356f4
Todd Stuke <tddstuke@gmail.com>
2022-04-09 19:26:58 -0500

    added datepicker and moment.js/added color alerts for dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,9 @@ var createTask = function (taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
+
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -88,12 +91,20 @@ $(".list-group").on("click", "span", function () {
     .val(date);
   // swap out e(lements
   $(this).replaceWith(dateInput);
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function () {
+      // when calendar is closed, force a "change" event on the "dateInput"
+      $(this).trigger("change");
+    },
+  });
   // automantically focus on the new element
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function () {
+$(".list-group").on("change", "input[type='text']", function () {
   // get current text
   var date = $(this).val().trim();
   // get the parent of ul's id attribute
@@ -109,7 +120,27 @@ $(".list-group").on("blur", "input[type='text']", function () {
     .text(date);
   // replace input with span element
   $(this).replaceWith(taskSpan);
+  // pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
+
+var auditTask = function (taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert to a moment bject at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
 
 // make lists sortable
 $(".card .list-group").sortable({
@@ -164,6 +195,11 @@ $("#trash").droppable({
   out: function (event, ui) {
     console.log("out");
   },
+});
+
+// adding date picker
+$("#modalDueDate").datepicker({
+  minDate: 1,
 });
 
 // save button in modal was clicked
