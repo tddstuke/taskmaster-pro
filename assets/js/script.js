963412e5f0559dca66152ad6d27f92bb4cb31cd6
Todd Stuke <tddstuke@gmail.com>
2022-04-11 21:17:26 -0500

    ux enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -148,13 +148,21 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  activte: function (event) {
+  activate: function (event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
     console.log("activate", this);
   },
   deactivate: function (event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
     console.log("over", event.target);
   },
+  over: function (event) {
+    $(event.target).addClass("dropover-active");
+  },
   out: function (event) {
+    $(event.target).removeClass("dropover-active");
     console.log("out", event.target);
   },
   update: function (event) {
@@ -187,12 +195,15 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function (event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
     console.log("drop");
   },
   over: function (event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
     console.log("over");
   },
   out: function (event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
     console.log("out");
   },
 });
@@ -203,7 +214,7 @@ $("#modalDueDate").datepicker({
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -232,6 +243,13 @@ $("#remove-tasks").on("click", function () {
   }
   saveTasks();
 });
+
+// timer
+setInterval(function () {
+  $(".card .list-group-item").each(function (index, el) {
+    auditTask(el);
+  });
+}, 1000 * 60 * 30);
 
 // load tasks for the first time
 loadTasks();
